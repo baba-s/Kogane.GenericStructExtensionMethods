106c5fb65a18ca1f1ba1302f37df03b3ebb02a24
baba-s <shotababa1206@gmail.com>
2022-10-15 09:30:15 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.GenericStructExtensionMethods
+# Kogane Generic Struct Extension Methods
+
+構造体の拡張メソッド
---
+++ Runtime/GenericStructExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace Kogane
+{
+    public static class GenericStructExtensionMethods
+    {
+        public static void Swap<T>( ref this T a, ref T b ) where T : struct
+        {
+            ( a, b ) = ( b, a );
+        }
+    }
+}
